f4cd87693345a7e3502a0f3f2f5280e80d16af5d
Geovanni Santos <117683781+Geovannydynoo@users.noreply.github.com>
2023-07-14 20:03:14 -0300

    add react new

---
+++ src/components/Header/Styles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 150px;
+  text-align: center;
+  background: #B87333;
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+  padding-top: 20px;
+  color: #00000;
+`;
